b89ddc5eeaa461ab981424d85b322c32802c9e9a
Daniel Jeffery <danialajeffery@gmail.com>
2020-09-12 15:08:40 -0700

    Working rounds

--- emoji_js13k/src/components/card.jsx
+++ emoji_js13k/src/components/card.jsx
@@ -1,33 +1,38 @@
 'use strict';
 
 // Don't forget to include h renderer!
-import {h, Component} from 'preact';
+import {h, Component, createRef} from 'preact';
 
 
 export default class Card extends Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
       hiddenValue: null,
       flipStatus: DEFAULT,
     };
+    this.socket = props.socket;
+
+    this.bind();
   }
 
-  reset() {
-    setTimeout(() => {
-      this.setState({flipStatus: DEFAULT, hiddenValue: null})
-    }, 2000);
+  bind() {
+    this.socket.on("newRound", () => {
+      setTimeout(() => {
+        this.setState({flipStatus: DEFAULT, hiddenValue: null})
+      }, 1000);
+    });
   }
 
-  flip(socket, val) {
-    socket.emit("flip", {"guess": val}, (cardFace) => {
+  flip(val) {
+    this.socket.emit("flip", {"guess": val}, (cardFace) => {
       this.setState({flipStatus: FLIPPED, hiddenValue: cardFace.emoji});
     });
   };
 
   render(props, state) {
     return (<div class="card" data-flipStatus={this.state.flipStatus} data-player={props.player}>
-      <button class="front" onClick={this.flip.bind(this, props.socket, props.val)} disabled={props.disabled}>
+      <button class="front" onClick={this.flip.bind(this, props.val)} disabled={props.disabled}>
             ?
       </button>
       <div className="back">{state.hiddenValue}</div>
